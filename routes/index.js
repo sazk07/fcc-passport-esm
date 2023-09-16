be17dc9ef5ef1a23ec354ef890b2590a3dba84fe
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2023-09-16 20:40:07 +0500

    tried using app.use() and removing next from homepage router

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 const router = Router()
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   return res.render('index', {
     title: 'Connected to Database',
     message: 'Please log in'
